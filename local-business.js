0d7fe0a1f41ed3fb2595b052daf6bb9c3cfeda85
Nguyễn Văn Quyết Định <55710339+dinhnvq@users.noreply.github.com>
2019-11-28 12:20:58 +0700

    Add files via upload

---
+++ local-business.js
@@ -0,0 +1,65 @@
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "LocalBusiness",
+  "image": "https://mingital.com/dich-vu-seo-mingital.png",
+  "@id": "https://mingital.com",
+  "name": "Dịch vụ SEO website tổng thể - SEO TOP #1 Google - MINGITAL",
+  "priceRange": "15.000.000 - 200.000.000 VND",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "32 Phước Lý 17",
+    "addressLocality": "Liên Chiểu",
+    "addressRegion": "Đà Nẵng",
+    "postalCode": "550000",
+    "addressCountry": "VN"
+  },
+  "review": {
+    "@type": "Review",
+    "reviewRating": {
+      "@type": "Rating",
+      "ratingValue": "4.9",
+      "bestRating": "5"
+    },
+    "author": {
+      "@type": "Person",
+      "name": "Ms Mai"
+    }
+  },
+  "aggregateRating": {
+    "@type": "AggregateRating",
+    "ratingValue": "4.9",
+    "ratingCount": "20",
+    "bestRating": "5",
+    "worstRating": "1"
+  },
+  "geo": {
+    "@type": "GeoCoordinates",
+    "latitude": 16.0446036,
+    "longitude": 108.1632023
+  },
+  "url": "https://www.google.com/maps/place/D%E1%BB%8Bch+v%E1%BB%A5+SEO+website+t%E1%BB%95ng+th%E1%BB%83+-+SEO+TOP+%231+Google+-+MINGITAL/@16.0446036,108.1632023,17z/data=!3m1!4b1!4m5!3m4!1s0x3142195c7f606181:0x4a7bab574ac835d7!8m2!3d16.0446036!4d108.165391
+",
+  "telephone": "0968399646",
+  "openingHoursSpecification": [
+    {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": [
+        "Monday",
+        "Tuesday",
+		"Wednesday",
+        "Thursday",
+        "Friday"
+      ],
+      "opens": "08:00",
+      "closes": "17:00"
+    },
+    {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": "Saturday",
+      "opens": "08:00",
+      "closes": "11:30"
+    }
+  ]
+}
+</script>
