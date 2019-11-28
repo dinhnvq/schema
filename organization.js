173a203beb819a5a339b16b27c90a930b173f7ce
Nguyễn Văn Quyết Định <55710339+dinhnvq@users.noreply.github.com>
2019-11-28 12:18:08 +0700

    Add files via upload

---
+++ organization.js
@@ -0,0 +1,40 @@
+<script type="application/ld+json">
+ { "@context": "https://schema.org",
+ "@type": "Organization",
+ "name": "Dịch vụ SEO website tổng thể - SEO TOP #1 Google - MINGITAL",
+ "legalName" : "MINGITAL",
+ "url": "https://mingital.com",
+ "logo": "https://mingital.com/logo-mingital.png",
+ "foundingDate": "2019",
+ "founders": {
+ "@type": "Person",
+ "name": "Nguyễn Văn Quyết Định"
+ },
+ "address": {
+ "@type": "PostalAddress",
+ "streetAddress": "32 Phước Lý 17",
+ "addressLocality": "Liên Chiểu",
+ "addressRegion": "Đà Nẵng",
+ "postalCode": "550000",
+ "addressCountry": "VN"
+ },
+ "contactPoint": {
+ "@type": "ContactPoint",
+ "contactType": "customer support",
+ "telephone": "+84968399646",
+ "email": "mingitalcorp@gmail.com"
+ },
+ "sameAs": [ 
+ "http://www.facebook.com/mingital",
+ "http://www.twitter.com/mingital",
+ "https://mingital.business.site",
+ "https://about.me/mingital",
+ "https://www.slideshare.net/mingital",
+ "https://www.instagram.com/mingitalcorp/",
+ "https://vi.gravatar.com/mingital",
+ "https://www.behance.net/mingital",
+ "https://www.quora.com/profile/Mingital",
+ "https://www.reddit.com/user/mingital",
+ "http://www.linkedin.com/company/mingital"
+ ]}
+</script>
