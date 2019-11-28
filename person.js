306fe7a9f67bd8bb290ff0daa913f26ed3c17ffe
Nguyễn Văn Quyết Định <55710339+dinhnvq@users.noreply.github.com>
2019-11-28 12:19:03 +0700

    Add files via upload

---
+++ person.js
@@ -0,0 +1,39 @@
+
+<script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "Person",
+      "address": {
+        "@type": "PostalAddress",
+        "addressLocality": "Liên Chiểu",
+        "addressRegion": "Đà Nẵng",
+        "postalCode": "550000",
+        "streetAddress": "32 Phước Lý 17"
+      },
+      "email": "nguyenvanquyetdinh@gmail.com",
+      "image": "https://mingital.com/nguyen-van-quyet-dinh.jpg",
+      "jobTitle": "Founder",
+      "worksFor": {
+      		"@type": "Organization",
+            "name": "Dịch vụ SEO website tổng thể - SEO TOP #1 Google - MINGITAL",
+            "url": "https://mingital.com"
+            },
+      "name": "Nguyễn Văn Quyết Định",
+      "alternateName": "dinhnvq",
+      "birthPlace": "Đà Nẵng, VN",
+      "gender": "female",
+      "nationality": "Vietnamese",
+      "telephone": "0968399646",
+	  "sameAs" : [ "https://www.facebook.com/dinhnvq",
+      "http://twitter.com/dinhnvq",
+      "https://www.linkedin.com/in/dinhnvq",
+      "https://www.reddit.com/user/dinhnvq",
+      "https://www.youtube.com/channel/UCAFM-n_tXIL4m9WDDTR5KQQ/about",
+      "https://about.me/dinhnvq",
+      "https://medium.com/@dinhnvq",
+      "https://www.pinterest.com/dinhnvq",
+      "https://www.behance.net/dinhnvq",
+      "https://www.slideshare.net/dinhnvq"
+	  ]
+    }
+</script>
